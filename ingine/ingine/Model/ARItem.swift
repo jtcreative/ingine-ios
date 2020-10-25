f1273590d50a5ef12e811de590dec394c9561818
Manish Dadwal <manishdadwal@Manishs-MacBook-Pro.local>
2020-10-25 23:42:33 +0530

    merged following

---
+++ ingine/ingine/Model/ARItem.swift
@@ -0,0 +1,23 @@
+//
+//  ARItem.swift
+//  ingine
+//
+//  Created by Manish Dadwal on 08/10/20.
+//  Copyright © 2020 ingine. All rights reserved.
+//
+
+import Foundation
+struct IngineeredItem {
+    var id = ""
+    var refImage = ""
+    var itemName = ""
+    var itemURL = ""
+    var visStatus = false
+}
+struct ARItem:Codable {
+    var id :String?
+    var name:String?
+    var refImage:String?
+    var matchURL:String?
+    var `public` :Bool?
+}
---
+++ ingine/ingine/Model/Following.swift
@@ -0,0 +1,17 @@
+//
+//  Following.swift
+//  ingine
+//
+//  Created by Manish Dadwal on 07/10/20.
+//  Copyright © 2020 ingine. All rights reserved.
+//
+
+import Foundation
+struct Following {
+    var fullName:String
+    var id:String
+    var profileImage:String
+    var isFollowing: Bool = false
+    var isFollowers : Bool = false
+    var assetCount = 0
+}
